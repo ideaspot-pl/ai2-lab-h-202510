87ec3a2ae1fd23a909d353756a997759f12719d2
Artur Karczmarczyk <artur@ideaspot.pl>
2025-11-07 00:11:25 +0100

    step 7 - api

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -13,8 +13,11 @@ export class Details {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(Housing);
   housingLocation: HousingLocationInfo | undefined;
+
   constructor() {
-    const housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+    const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
+    this.housingService.getHousingLocationById(housingLocationId).then((housingLocation) => {
+      this.housingLocation = housingLocation;
+    });
   }
 }
--- src/app/housing.ts
+++ src/app/housing.ts
@@ -5,45 +5,16 @@ import {HousingLocationInfo} from './housinglocation';
   providedIn: 'root',
 })
 export class Housing {
-  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+  url = 'http://localhost:12346/locations';
 
-  housingLocationList: HousingLocationInfo[] = [
-    {
-      id: 0,
-      name: 'Acme Fresh Start Housing',
-      city: 'Chicago',
-      state: 'IL',
-      photo: `${this.baseUrl}/bernard-hermant-CLKGGwIBTaY-unsplash.jpg`,
-      availableUnits: 4,
-      wifi: true,
-      laundry: true,
-    },
-    {
-      id: 1,
-      name: 'A113 Transitional Housing',
-      city: 'Santa Monica',
-      state: 'CA',
-      photo: `${this.baseUrl}/brandon-griggs-wR11KBaB86U-unsplash.jpg`,
-      availableUnits: 0,
-      wifi: false,
-      laundry: true,
-    },
-    {
-      id: 2,
-      name: 'Warm Beds Housing Support',
-      city: 'Juneau',
-      state: 'AK',
-      photo: `${this.baseUrl}/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg`,
-      availableUnits: 1,
-      wifi: false,
-      laundry: false,
-    },
-  ];
+async getAllHousingLocations(): Promise<HousingLocationInfo[]> {
+  const data = await fetch(this.url);
+  return (await data.json()) ?? [];
+}
 
-  getAllHousingLocations(): HousingLocationInfo[] {
-    return this.housingLocationList;
-  }
-  getHousingLocationById(id: number): HousingLocationInfo | undefined {
-    return this.housingLocationList.find((housingLocation) => housingLocation.id === id);
-  }
+async getHousingLocationById(id: number): Promise<HousingLocationInfo | undefined> {
+  const data = await fetch(`${this.url}?id=${id}`);
+  const locationJson = await data.json();
+  return locationJson[0] ?? {};
+}
 }
